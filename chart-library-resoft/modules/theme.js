773ab5f94be3dd2bb81e8a2257b8dc1a4a114109
tanghuan <18601376034@163.com>
2021-08-10 14:39:55 +0800

    初始化

---
+++ chart-library-resoft/modules/theme.js
@@ -0,0 +1,28 @@
+import echarts from 'echarts'
+import _ from 'lodash'
+// 配置图表主题
+import customed from '../json/customed'
+import theme4 from '../json/theme4'
+var setChartTheme = function (themeType, opts) {
+    var _this = this
+    var themes = {
+        customed: customed,
+        theme4: theme4
+    }
+    var data = themes[themeType]
+    if (data) {
+        _this.colors = data.color
+        _this.barColors = data.barColors
+        _this.barStackColors = data.barStackColors
+        _this.pieLabelColor = data.pieLabelColor
+        _this.shieldColor = data.shieldColor
+        _this.shieldTextColor = data.shieldTextColor
+        _this.shieldValueColor = data.shieldValueColor
+        _this.shieldLineColor = data.shieldLineColor
+        if (!opts.type || opts.type === 'echarts') {
+            echarts.registerTheme(themeType, data);
+            _this.chart = echarts.init(document.getElementById(opts.id), themeType);
+        }
+    }
+}
+export default setChartTheme
---
+++ chart-library-resoft/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  entry: __dirname+ "/index.js",
+  mode: "development",
+  output:{
+    path: __dirname + "/dist",
+    filename: 'chartFactory.min.js'
+  }
+}
